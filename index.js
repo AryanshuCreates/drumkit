9c57b9d50ea6f62d78800944980efdf75b03dd9d
Aryanshu Aman <120834152+AryanshuCreates@users.noreply.github.com>
2023-11-21 00:39:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('w').addEventListener('click', function() {
+    var audio = document.getElementById('wdrum');
+    audio.play();
+  });
+
+  document.getElementById('a').addEventListener('click', function() {
+    var audio = document.getElementById('adrum');
+    audio.play();
+  });
+  document.getElementById('s').addEventListener('click', function() {
+    var audio = document.getElementById('sdrum');
+    audio.play();
+  });
+  document.getElementById('d').addEventListener('click', function() {
+    var audio = document.getElementById('ddrum');
+    audio.play();
+  });
+  document.getElementById('j').addEventListener('click', function() {
+    var audio = document.getElementById('jdrum');
+    audio.play();
+  });
+  document.getElementById('k').addEventListener('click', function() {
+    var audio = document.getElementById('kdrum');
+    audio.play();
+  });
+  document.getElementById('l').addEventListener('click', function() {
+    var audio = document.getElementById('ldrum');
+    audio.play();
+  });
+  
